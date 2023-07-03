17830fce0d46bcfa4b447a292ed664cdc7055c5d
ArtemOsypov <artem.osypov@gmail.com>
2023-07-04 01:50:55 +0300

    corr#2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,11 @@ import { ContactsPage } from 'pages/Contacts';
 import { refreshUser } from './redux/auth/operations';
 import { RestrictedRoute } from './RestrictedRoute';
 import { PrivateRoute } from './PrivateRoute';
+import {useAuth } from '../hooks/useAuth';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -19,23 +21,26 @@ export const App = () => {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/register"
-            element={<RestrictedRoute component={RegisterPage} redirectTo="/contacts" />}
-          />
-          <Route
-            path="/login"
-            element={<RestrictedRoute component={LoginPage} redirectTo="/contacts" />}
-          />
-          <Route
-            path="/contacts"
-            element={<PrivateRoute component={ContactsPage} redirectTo="/login" />}
-          />
-        </Route>
-      </Routes>
+      {!isRefreshing && (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route
+              path="/register"
+              element={<RestrictedRoute component={RegisterPage} redirectTo="/contacts" />}
+            />
+            <Route
+              path="/login"
+              element={<RestrictedRoute component={LoginPage} redirectTo="/contacts" />}
+            />
+            <Route
+              path="/contacts"
+              element={<PrivateRoute component={ContactsPage} redirectTo="/login" />}
+            />
+          </Route>
+        </Routes>
+      )
+      }
     </>
   );
 };
